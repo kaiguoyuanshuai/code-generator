a3b3d59c7c2cb84e5ee0126ec3c3d9f22375ccfa
zcoco <zheng_coco@yeah.net>
2018-12-21 17:28:49 +0800

    修改成阿里巴巴提供的velocity视图

--- src/main/java/com/zcoco/code/codegenerator/CodeGeneratorApplication.java
+++ src/main/java/com/zcoco/code/codegenerator/CodeGeneratorApplication.java
@@ -1,15 +1,9 @@
 package com.zcoco.code.codegenerator;
 
 import org.apache.velocity.app.VelocityEngine;
-import org.apache.velocity.runtime.RuntimeConstants;
-import org.apache.velocity.runtime.resource.loader.ClasspathResourceLoader;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import java.io.IOException;
-import java.util.Properties;
 
 @SpringBootApplication
 public class CodeGeneratorApplication {
@@ -20,16 +14,6 @@ public class CodeGeneratorApplication {
     public static void main(String[] args) {
         SpringApplication.run(CodeGeneratorApplication.class, args);
     }
-
-/*
-    @Bean
-    VelocityEngine velocityEngine() throws IOException {
-        Properties properties = new Properties();
-        properties.load(this.getClass().getResourceAsStream("/application.properties"));
-        properties.setProperty(RuntimeConstants.RESOURCE_LOADER, "classpath");
-        properties.setProperty("classpath.resource.loader.class", ClasspathResourceLoader.class.getName()) ;
-        return new VelocityEngine(properties);
-    }*/
 
 }
 
